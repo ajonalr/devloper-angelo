4b003e7e43221dad47731db1440ca43a64ddc1c3
Ariel Ramirez <33263187+ajonalr@users.noreply.github.com>
2026-06-16 09:16:41 -0600

    cambios

--- backend_proyecto/src/app.controller.ts
+++ backend_proyecto/src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('profile')
+  getProfile() {
+    return this.appService.getProfile()
+  }
+
 }
--- backend_proyecto/src/app.service.ts
+++ backend_proyecto/src/app.service.ts
@@ -3,9 +3,26 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+
+
   constructor(private configService: ConfigService) {}
+
+ 
 
   getHello(): string {
     return this.configService.getOrThrow<string>('APP_NAME');
   }
+
+
+  getProfile() {
+
+    return  {
+      'name': 'Ariel Ramirez',
+      'eage': 26,
+      'address' : 'ciudad'
+    }
+
+  }
+
+
 }
